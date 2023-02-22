64d3dafb28ecf3c9132748a3c8ecfaadcfe0af85
YanCoder <khoant20@uef.edu.vn>
2023-02-22 13:28:13 +0700

    Add project files.

---
+++ UserRole/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserRole.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
